f74d9cf0e1a30636a6b4ea3b8fc15b1968325c45
yaelshtaiman <yaelshtaiman@gmail.com>
2025-05-05 14:06:55 +0300

    changing the layout

--- src/components/Bookshelves.js
+++ src/components/Bookshelves.js
@@ -2,23 +2,36 @@
 const Bookshelves = ({readingList}) => {
     return (
         <div id="bookshelves" className="bookshelves">
-            {Object.entries(readingList).map(([shelfName, books]) => (
-                <div key={shelfName} className="shelf-selction">
-                    <h2>{shelfName}</h2>
-                    {books.length === 0 ? (
-                        <p>No books in this shelf.</p>
-                    ) : (
-                        <ul className="book-list">
-                            {books.map((book) => (
-                                <li key={book.id} className="book-item">
-                                    <h4>{book.volumeInfo.title}</h4>
-                                    <h4>{book.volumeInfo.authors?.join(", ")}</h4>
-                                </li>
-                            ))}
-                        </ul>
-                    )}
-                </div>
-            ))}
+            <div className="shelf-container">
+                <h3>Bookshelves</h3>
+
+                {Object.entries(readingList).map(([shelfName, books]) => (
+                    <div key={shelfName} className="shelf-selection">
+                        <div className="shelf-count">
+                            {books.length}
+                        </div>
+                        <div className="shelf-name">
+                            {shelfName}
+                        </div>
+                        {/*<div>*/}
+                        {/*    {books.length} {shelfName}*/}
+                        {/*</div>*/}
+                        {/*{books.length === 0 ? (*/}
+                        {/*    <p>No books in this shelf.</p>*/}
+                        {/*) : (*/}
+                        {/*    <ul className="book-list">*/}
+                        {/*        {books.map((book) => (*/}
+                        {/*            <li key={book.id} className="book-item">*/}
+                        {/*                <h4>{book.volumeInfo.title}</h4>*/}
+                        {/*                <h4>{book.volumeInfo.authors?.join(", ")}</h4>*/}
+                        {/*            </li>*/}
+                        {/*        ))}*/}
+                        {/*    </ul>*/}
+                        {/*)}*/}
+                    </div>
+                ))}
+            </div>
+
         </div>
     );
 };
