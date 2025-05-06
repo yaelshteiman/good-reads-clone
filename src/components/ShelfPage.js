927bda92daf0f96a35ef75c020fcbdf0f5830795
yaelshtaiman <yaelshtaiman@gmail.com>
2025-05-06 19:35:43 +0300

    updating ShelfPage.js - making a table and designing it. the table includes: 1. # 2. cover 3. title 4. author  + there is a remove from the list button. when clicking it the user will have to confirm the remove

--- src/components/ShelfPage.js
+++ src/components/ShelfPage.js
@@ -1,23 +1,50 @@
 import { useParams } from "react-router-dom";
 
-const ShelfPage = ({ readingList }) => {
+const ShelfPage = ({ readingList, handleSelectList}) => {
     const { shelfName } = useParams(); // Extract shelfName from the object returned by useParams
     const books = readingList[shelfName] || []; // Use shelfName directly
 
     return (
         <div id="shelfPage" className="shelf-page">
-            <h2>{shelfName}</h2>
+            <h2>{shelfName} {books.length === 0 ? ("") : (`(${books.length})`)}</h2>
             {books.length === 0 ? (
                 <p>No books in this shelf.</p>
             ) : (
-                <ul className="book-list">
-                    {books.map((book) => (
-                        <li key={book.id} className="book-item">
-                            <h4>{book.volumeInfo.title}</h4>
-                            <h4>{book.volumeInfo.authors?.join(", ")}</h4>
-                        </li>
+                <table className="books-table">
+                    <thead>
+                        <tr className="table-list">
+                            <th>#</th>
+                            <th>cover</th>
+                            <th>title</th>
+                            <th>author</th>
+                        </tr>
+                    </thead>
+                    <tbody className="books-body">
+                    {books.map((book, index) => (
+                        <tr key={book.id} className="book-a-like">
+                            <td className="field-position">{index+1}</td>
+                            <td className="field-cover">
+                                <img
+                                    src={book.volumeInfo.imageLinks && book.volumeInfo.imageLinks.smallThumbnail}
+                                    alt=""
+                                />
+                            </td>
+                            <td className="field-title">{book.volumeInfo.title}</td>
+                            <td className="field-author">{book.volumeInfo.authors?.join(", ")}</td>
+                            <td className="field-action">
+                                <button className="delete-book-a-like" onClick={() => {
+                                    const confirmed = window.confirm(`Are you sure you want to remove ${book.volumeInfo.title} from your books? This will permanently remove this book from your shelves.`)
+                                    if (confirmed) {
+                                        handleSelectList(book, null);
+                                    }
+                                }}>
+                                    <i className="fa-duotone fa-solid fa-xmark"></i>
+                                </button>
+                            </td>
+                        </tr>
                     ))}
-                </ul>
+                    </tbody>
+                </table>
             )}
         </div>
     );
